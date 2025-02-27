443de329a3716fdb4d7a9dea9464c8d731d679cc
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-02-27 09:36:54 +0100

    47. Configure Kafka Consumer using SpringBoot Profiles - application.yaml

---
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,20 @@
+package com.learnkafka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+/**
+ * LibraryEventsConsumerConfig
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - pvraul
+ * @version 27/02/2025 - 09:34
+ * @since 1.17
+ */
+@Configuration
+public class LibraryEventsConsumerConfig {
+
+
+
+}
